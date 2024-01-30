45b1e9bee45a50f09e751a77055c4bbd8d0afb6a
tmnpierre <tomyn.pierre@gmail.com>
2024-01-30 10:06:19 +0100

    feat(Exercice01): completed exercice

--- Exercie01Contacts/Controllers/ContactsController.cs
+++ Exercie01Contacts/Controllers/ContactsController.cs
@@ -4,6 +4,16 @@ namespace Exercie01Contacts.Controllers
 {
     public class ContactsController : Controller
     {
+        private int ID { get; set; }
+        private string Name { get; set; }
+        private string Email { get; set; }
+
+        public ContactsController(int id, string name, string email)
+        {
+            ID = id;
+            Name = name;
+            Email = email;
+        }
         public IActionResult Index()
         {
             return View();
@@ -11,6 +21,12 @@ namespace Exercie01Contacts.Controllers
 
         public IActionResult Details()
         {
+            List<ContactsController> contacts = new List<ContactsController>();
+            {
+                contacts.Add(new ContactsController(ID = 1, Name = "Jean Bon", Email = "jeanbon@emial.fr"));
+                contacts.Add(new ContactsController(ID = 2, Name = "Bernard Lermitte", Email = "bernardlermitte@emial.fr"));
+            }
+
             return View();
         }
 
