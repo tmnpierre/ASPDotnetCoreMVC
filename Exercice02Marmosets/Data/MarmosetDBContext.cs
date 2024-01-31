bbddd240554574b24cc86eb2346421d8a3efb57c
tmnpierre <tomyn.pierre@gmail.com>
2024-01-31 15:25:04 +0100

    feat(Exercice02): update controller MarmosetsController

---
+++ Exercice02Marmosets/Data/MarmosetDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Exercice02Marmosets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exercice02Marmosets.Data
+{
+    public class MarmosetDBContext : DbContext
+    {
+        public DbSet<Marmoset>? Marmosets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source=(localdb)\\MSSQLLocalDB; Database=exerciceMarmosetsDB;");
+        }
+    }
+}
