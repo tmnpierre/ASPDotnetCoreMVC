7f04f868837a601b08d5fd4c82e11a5448e0a748
tmnpierre <tomyn.pierre@gmail.com>
2024-01-31 11:45:04 +0100

    feat(Exercice02): update controller MarmosetsController

--- Exercice02Marmosets/Controllers/MarmosetsController.cs
+++ Exercice02Marmosets/Controllers/MarmosetsController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Exercice02Marmosets.Models;
+using Microsoft.AspNetCore.Mvc;
+using Exercice02Marmosets.Data;
 
 namespace Exercice02Marmosets.Controllers
 {
-    public class MarmosetsController : Controller
+    public class ContactsController : Controller
     {
+        private FakeMarmosetDB _fakeMarmosetDB;
+
+        public MarmosetsController(FakeMarmosetDB fakeMarmosetDB)
+        {
+            _fakeMarmosetDB = fakeMarmosetDB;
+        }
+
         public IActionResult Index()
         {
+            var marmoset = _fakeMarmosetDB.GetAll();
+            return View(marmoset);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var marmoset = _fakeMarmosetDB.GetById(id);
+            if (marmoset == null)
+            {
+                return NotFound();
+            }
+
+            return View(marmoset);
+        }
+
+        public IActionResult Add()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Marmoset newMarmoset)
+        {
+            if (ModelState.IsValid)
+            {
+                _fakeMarmosetDB.Add(newMarmoset);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(newMarmoset);
         }
     }
 }
