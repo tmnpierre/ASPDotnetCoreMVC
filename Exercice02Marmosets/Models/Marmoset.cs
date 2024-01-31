f6ff161988dbd6051b3632a68c2081289c1c28ea
tmnpierre <tomyn.pierre@gmail.com>
2024-01-31 11:14:47 +0100

    feat(Exercice02): update model Marmoset.cs

--- Exercice02Marmosets/Models/Marmoset.cs
+++ Exercice02Marmosets/Models/Marmoset.cs
@@ -1,6 +1,26 @@
-﻿namespace Exercice02Marmosets.Models
+﻿using System.ComponentModel;
+
+namespace Exercice02Marmosets.Models
 {
     public class Marmoset
     {
+        public int Id { get; set; }
+
+        [DisplayName("Nom")]
+        public string Name { get; set; }
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [DisplayName("Age")]
+        public int Age { get; set; }
+
+        public Marmoset(int id, string name, string descripton, int age)
+        {
+            Id = id;
+            Name = name;
+            Description = descripton;
+            Age = age;
+        }
     }
 }
