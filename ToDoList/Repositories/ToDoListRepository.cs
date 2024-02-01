759f81fb3c46fa6b38abaff5f8ac0b6700616360
tmnpierre <tomyn.pierre@gmail.com>
2024-02-01 15:28:44 +0100

    ToDoList

--- ToDoList/Repositories/ToDoListRepository.cs
+++ ToDoList/Repositories/ToDoListRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace ToDoList.Repositories
+﻿using ToDoList.Data;
+using ToDoList.Models;
+using System.Linq.Expressions;
+
+namespace ToDoList.Repositories
 {
-    public class ToDoListRepository
+    public class ToDoListRepository : IToDoListRepository
     {
+        public bool Add(ToDoLists tache)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ToDoLists? Get(Expression<Func<ToDoLists, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ToDoLists> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ToDoLists> GetAll(Expression<Func<ToDoLists, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ToDoLists? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(ToDoLists animal)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
