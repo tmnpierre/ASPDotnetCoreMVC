a19828177e6b171a6e6b3c9ff23269ff1df6444d
tmnpierre <tomyn.pierre@gmail.com>
2024-02-01 15:11:30 +0100

    ToDoList

---
+++ ToDoList/Repositories/IRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace ToDoList.Repositories
+{
+    public interface IRepository
+    {
+    }
+}
---
+++ ToDoList/Repositories/ToDoListRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace ToDoList.Repositories
+{
+    public class ToDoListRepository
+    {
+    }
+}
