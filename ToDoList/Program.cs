19027708875765477f27ba7b3f13e64f6c0c650a
tmnpierre <tomyn.pierre@gmail.com>
2024-02-01 16:45:56 +0100

    ToDoList

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,14 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoList.Data;
+using ToDoList.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDatabase")));
+
+builder.Services.AddScoped<ITacheRepository, TacheRepository>();
 
 var app = builder.Build();
 
