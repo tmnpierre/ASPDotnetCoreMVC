da85b6740d4b5846ebf8b3a64d2393f856c24fe9
tmnpierre <tomyn.pierre@gmail.com>
2024-02-01 15:18:32 +0100

    ToDoList

---
+++ ToDoList/Models/ToDoList.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Titre de la tâche")]
+        public string Name { get; set; }
+
+        [Display(Description = "Description de la tâche")]
+        public string Description { get; set; }
+    }
+}
--- ToDoList/Repositories/IRepository.cs
+++ ToDoList/Repositories/IRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace ToDoList.Repositories
+﻿using ToDoList.Models;
+using System.Linq.Expressions;
+
+namespace ToDoList.Repositories
 {
-    public interface IRepository
+    public interface IRepository<TEntity> 
     {
+        bool Add(TEntity tache);
+        TEntity? Get(Expression<Func<TEntity, bool>> predicate);
+        List<TEntity> GetAll();
+        List<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
+        TEntity? GetById(int id);
+        bool Update(TEntity tache);
+        bool Delete(int id);
     }
 }
