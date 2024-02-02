d15376dfa5bcda1ae46d1031c13d56e0ca7ab431
tmnpierre <tomyn.pierre@gmail.com>
2024-02-02 16:16:24 +0100

    feat(model): add Categorie.cs

---
+++ ECommerce/Models/Categorie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models
+{
+    public class Categorie
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nom de la catégorie")]
+        [Required(ErrorMessage = "Le nom de la catégorie est requis.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Le nom de la catégorie doit être compris entre 3 et 50 caractères.")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Liste de produits de la catégorie")]
+        public List<Product>? Products { get; set; }
+    }
+}
