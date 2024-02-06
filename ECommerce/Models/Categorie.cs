0643598d1a56d5f45d7ab513466720ba3169b77d
tmnpierre <tomyn.pierre@gmail.com>
2024-02-06 12:06:28 +0100

    feat(data): update for migration

--- ECommerce/Models/Categorie.cs
+++ ECommerce/Models/Categorie.cs
@@ -9,7 +9,7 @@ namespace ECommerce.Models
         [Display(Name = "Nom de la catégorie")]
         [Required(ErrorMessage = "Le nom de la catégorie est requis.")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Le nom de la catégorie doit être compris entre 3 et 50 caractères.")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         public virtual ICollection<Product> Products { get; set; }
 
