f82cf9231609896866eaf11c11cf981bd43e98a1
tmnpierre <tomyn.pierre@gmail.com>
2024-02-06 12:13:39 +0100

    feat(core): architecture update

---
+++ ECommerce/Controllers/SalesBasketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Controllers
+{
+    public class SalesBasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
